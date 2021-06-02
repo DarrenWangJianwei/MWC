98c5ba1e85c6d57057ba99d3db31a0800f57e208
darren.wjy1990@gmail.com <Darren.wjy1990@gmail.com>
2021-06-01 17:41:36 -0700

    Version 1.1, change header

---
+++ site/src/@elegantstack/solid-ui-theme/colors.js
@@ -0,0 +1,21 @@
+// Importing default theme color object to mutate it
+import defaultColors from '@elegantstack/solid-ui-theme/src/colors'
+
+// Using tailwind preset colors
+import colors from '@elegantstack/solid-ui-theme/src/color-preset'
+
+export default {
+  ...defaultColors,
+  // Alpha (primary)
+  alphaLighter: colors.blue[100],
+  alphaLight: colors.blue[300],
+  alpha: colors.blue[500],
+  alphaDark: colors.blue[600],
+  alphaDarker: colors.blue[800],
+  // beta (secondary)
+  betaLighter: colors.lime[100],
+  betaLight: colors.lime[300],
+  beta: colors.lime[500],
+  betaDark: colors.lime[700],
+  betaDarker: colors.lime[800]
+}
